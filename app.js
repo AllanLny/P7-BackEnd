0b5ecdd3504fc559c946fba823ab8e743d27d311
Allan Lannoy <allan1411lannoy@gmail.com>
2023-06-13 14:07:54 +0200

    Ajout Backend etapes 1.1 , 1.2

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('expres');
+const mongoose = require('mongoose');
+
+
+const UserRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://User:<UserPassword1411>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/auth', UserRoutes);
+
+module.exports = app;
---
+++ models/User.js
@@ -0,0 +1,11 @@
+const { default: mongoose } = require("mongoose")
+const UniqueValidator = require("mongoose-unique-validator")
+
+const userSchema = mongoose.Schema({
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true }
+});
+
+userSchema.plugin(UniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
