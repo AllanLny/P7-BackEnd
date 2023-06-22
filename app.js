adf6326854dd4ac78028c7500a8b659b0b592933
Allan Lannoy <allan1411lannoy@gmail.com>
2023-06-22 12:48:11 +0200

    Add Express-Rate-Limit

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express');
-
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-
-
-
 const userRoutes = require('./routes/user');
 require('dotenv').config();
 const MongoDBUser = process.env.USER;
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, //10 minutes
+    max: 5, // Max par ip et par windowMs
+    message: "Limite atteinte !"
+});
 
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/signup", limiter, userCtrl.signup);
+router.post("/login", limiter, userCtrl.login);
 
 module.exports = router;
